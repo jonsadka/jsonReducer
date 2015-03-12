a4d8979dae328044fc09a79bb81e7d9ab7b0d75e
Jon Sadka <jon@lever.co>
2015-03-12 13:52:58 -0700

    Create json reducer file

---
+++ reducer.js
@@ -0,0 +1,16 @@
+// set the global variable
+var reducedData;
+
+// setup the jQuery function to fetch the local json file
+var modifyData = function(){
+  $.getJSON('data.json', function(json){
+    var result;
+
+    // modify data.json file and modify result;
+
+    // set the result to reduced data before leaving the callback
+    // write yourself a message indicating that the script is finished
+    console.log('All done making your data smaller and prettier!');
+    reducedData = result;
+  });
+};
